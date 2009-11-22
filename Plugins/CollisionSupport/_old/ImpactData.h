d01bdc39881b9821586302cdaf6e04757d435d36
shojiokamura <shojiokamura@eb6eb719-311c-42bc-8795-40e0810bf6b9>
2009-11-22 18:34:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/spindizzyremake/code@28 eb6eb719-311c-42bc-8795-40e0810bf6b9

---
+++ Plugins/CollisionSupport/_old/ImpactData.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2009 Martin Brentnall
+ *
+ * This file is part of Iso-Realms.
+ *
+ * Iso-Realms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Iso-Realms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Iso-Realms.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef IMPACT_DATA
+#define IMPACT_DATA
+
+#include "ImpactPoint.h"
+#include "PlaneSurface.h"
+#include "WallSurface.h"
+
+class ImpactData {
+  private:
+  ImpactPoint cLocation;
+  PlaneSurface *cSurface;
+  WallSurface *cWall;
+
+  public:
+  ImpactData(ImpactPoint location, PlaneSurface *surface);
+  ImpactData(ImpactPoint location, WallSurface *surface);
+  ImpactData(ImpactPoint location);
+  ImpactData();
+  ImpactPoint getLocation();
+  PlaneSurface* getSurface();
+  WallSurface* getWall();
+  int getType();
+  float getGradient();
+};
+
+#endif
