2893187bd1adc7a97eb45c6a856d203919b39681
shojiokamura <shojiokamura@eb6eb719-311c-42bc-8795-40e0810bf6b9>
2010-06-26 18:22:16 +0000

    git-svn-id: svn://svn.code.sf.net/p/spindizzyremake/code@98 eb6eb719-311c-42bc-8795-40e0810bf6b9

--- IsoRealms/DefaultZoneRenderer.cpp
+++
@@ -1,7 +0,0 @@
-#include "DefaultZoneRenderer.h"
-
-void DefaultZoneRenderer::render(std::vector<IZone*>& zones) {
-  for (unsigned int i = 0; i < zones.size(); i++) {
-    zones[i]->render();
-  }
-}
